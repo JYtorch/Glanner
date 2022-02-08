f804993fb9d56da60188258f771c7f9bf6a8bfd8
Lee, JeongUm(Leah) <aldlswjddma@naver.com>
2022-02-08 11:38:55 +0900

    S06P12A606-106 feat: 그룹게시판 제목+내용 검색 기능

--- backend/glanner/src/main/java/com/glanner/api/controller/GroupBoardController.java
+++ backend/glanner/src/main/java/com/glanner/api/controller/GroupBoardController.java
@@ -1,6 +1,7 @@
 package com.glanner.api.controller;
 
 import com.glanner.api.dto.request.SaveGroupBoardReqDto;
+import com.glanner.api.dto.request.SearchBoardReqDto;
 import com.glanner.api.dto.response.BaseResponseEntity;
 import com.glanner.api.dto.response.FindGroupBoardResDto;
 import com.glanner.api.exception.UserNotFoundException;
@@ -55,4 +56,9 @@ public class GroupBoardController extends BoardController<SaveGroupBoardReqDto> 
         return ResponseEntity.status(200).body(responseDto);
     }
 
+    @GetMapping("/search/{page}/{limit}")
+    public ResponseEntity<List<FindGroupBoardResDto>> searchBoards(@PathVariable int page, @PathVariable int limit, @RequestBody @Valid SearchBoardReqDto reqDto){
+        List<FindGroupBoardResDto> responseDto = queryRepository.findByKeyWord(page, limit, reqDto);
+        return ResponseEntity.status(200).body(responseDto);
+    }
 }
